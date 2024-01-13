371bc64f39b87d2b5047e3c28982fa9587f31aa8
abhikainthla <105478999+abhikainthla@users.noreply.github.com>
2024-01-13 14:36:32 +0530

    fixed loading issue

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ let text = document.getElementById("text");
 let container = document.querySelector("body");
 let toggle = document.querySelector("input");
 toggle.addEventListener('click',() => {
-    container.style.backgroundColor ="white";
-    if(text.style.color === "white"){
-        text.style.color="black";
+    container.style.backgroundColor ="black";
+    if(text.style.color != "white"){
+        text.style.color="white";
     }
     else{
-        text.style.color="white";
-        container.style.backgroundColor ="black";
+        text.style.color="black";
+        container.style.backgroundColor ="white";
     }
 })
